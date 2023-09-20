a458ef7ca2734a4746afab983643b39b7f66696c
feexon <feexon@foxmail.com>
2023-09-20 18:48:32 +0800

    Added: enable colorized output in tty mode only

--- src/lib.rs
+++ src/lib.rs
@@ -96,7 +96,7 @@ struct Execution<'a>(Plan<'a>, PathBuf);
 impl<'a> Execution<'a> {
     async fn run(&self) -> Result {
         use termcolor::{
-            Buffer, Color, ColorChoice, ColorSpec, HyperlinkSpec, StandardStream, WriteColor,
+            Buffer, BufferWriter, Color, ColorChoice, ColorSpec, HyperlinkSpec, WriteColor,
         };
         let result = self.0.run(&self.1).await;
         write(&result, &self.1)?;
@@ -104,21 +104,24 @@ impl<'a> Execution<'a> {
         return result;
 
         fn write(result: &Result, path: &Path) -> IOResult<()> {
-            let mut out = StandardStream::stdout(ColorChoice::Auto);
-            out.write_all(try_concat(tag(path), colorized(result))?.as_slice())?;
-            out.flush()
+            use std::io::{stdout, IsTerminal};
+            let out = BufferWriter::stdout(match stdout().is_terminal() {
+                true => ColorChoice::Always,
+                _ => ColorChoice::Never,
+            });
+            out.print(&try_concat(tag(path, &out), colorized(result, &out))?)
         }
 
-        fn try_concat(head: IOResult<Buffer>, tail: IOResult<Buffer>) -> IOResult<Vec<u8>> {
-            let tail = tail?.into_inner();
-            let mut buf = head?.into_inner();
-            buf.extend(tail);
-            buf.push(b'\n');
+        fn try_concat(head: IOResult<Buffer>, tail: IOResult<Buffer>) -> IOResult<Buffer> {
+            let mut buf = Buffer::ansi();
+            buf.write_all(head?.as_slice())?;
+            buf.write_all(tail?.as_slice())?;
+            buf.write_all(b"\n")?;
             Ok(buf)
         }
 
-        fn tag(path: &Path) -> IOResult<Buffer> {
-            let mut buf = Buffer::ansi();
+        fn tag(path: &Path, out: &BufferWriter) -> IOResult<Buffer> {
+            let mut buf = out.buffer();
             write!(buf, "clean: ")?;
             let url = format!("file://{}", path.display());
             buf.set_hyperlink(&HyperlinkSpec::open(url.as_bytes()))?;
@@ -128,8 +131,8 @@ impl<'a> Execution<'a> {
             Ok(buf)
         }
 
-        fn colorized(result: &Result) -> IOResult<Buffer> {
-            let mut buf = Buffer::ansi();
+        fn colorized(result: &Result, out: &BufferWriter) -> IOResult<Buffer> {
+            let mut buf = out.buffer();
             let (fg, text) = if let Ok(true) = result {
                 (Color::Green, "ok")
             } else {
