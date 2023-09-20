9f2dd44c42a2dffaac023e0518b3c76cb10efd18
feexon <feexon@foxmail.com>
2023-09-20 16:51:22 +0800

    Added: colorized clean output

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(io_error_other)]
 use std::{
+    io::Write,
     path::{Path, PathBuf},
     sync::Arc,
 };
@@ -94,17 +95,52 @@ struct Execution<'a>(Plan<'a>, PathBuf);
 
 impl<'a> Execution<'a> {
     async fn run(&self) -> Result {
+        use termcolor::{
+            Buffer, Color, ColorChoice, ColorSpec, HyperlinkSpec, StandardStream, WriteColor,
+        };
         let result = self.0.run(&self.1).await;
-        println!(
-            "clean: {} ? {}",
-            self.1.display(),
-            result
-                .as_ref()
-                .ok()
-                .filter(|ok| **ok)
-                .map(|_| "ok")
-                .unwrap_or("error")
-        );
-        result
+        write(&result, &self.1)?;
+
+        return result;
+
+        fn write(result: &Result, path: &Path) -> IOResult<()> {
+            let mut out = StandardStream::stdout(ColorChoice::Auto);
+            out.write_all(try_concat(tag(path), colorized(result))?.as_slice())?;
+            out.flush()
+        }
+
+        fn try_concat(head: IOResult<Buffer>, tail: IOResult<Buffer>) -> IOResult<Vec<u8>> {
+            let tail = tail?.into_inner();
+            let mut buf = head?.into_inner();
+            buf.extend(tail);
+            buf.push(b'\n');
+            Ok(buf)
+        }
+
+        fn tag(path: &Path) -> IOResult<Buffer> {
+            let mut buf = Buffer::ansi();
+            write!(buf, "clean: ")?;
+            let url = format!("file://{}", path.display());
+            buf.set_hyperlink(&HyperlinkSpec::open(url.as_bytes()))?;
+            write!(buf, "{}", path.display())?;
+            buf.set_hyperlink(&HyperlinkSpec::close())?;
+            write!(buf, "? ")?;
+            Ok(buf)
+        }
+
+        fn colorized(result: &Result) -> IOResult<Buffer> {
+            let mut buf = Buffer::ansi();
+            let (fg, text) = if let Ok(true) = result {
+                (Color::Green, "ok")
+            } else {
+                (Color::Red, "error")
+            };
+            let mut spec = ColorSpec::new();
+            buf.set_color(spec.set_fg(Some(fg)))?;
+            write!(buf, "{}", text)?;
+            spec.clear();
+            buf.set_color(&spec)?;
+            Ok(buf)
+        }
     }
 }
